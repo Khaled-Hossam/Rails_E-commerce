c74aea271da2cc14c58d02d13b7b09b5dfa87bc0
a-tarek <a.tarek.elsayed@gmail.com>
2019-04-28 12:03:56 +0200

    feat(categories): add db unique constraint to name

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_04_27_203929) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
