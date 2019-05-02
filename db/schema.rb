a1fa80f95e78d421d12c5177cf04dafe0d65477b
Mira <mira.ph.ling@gmail.com>
2019-05-02 13:39:45 +0200

    feat(tableusers)Add user coloumn role to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_192237) do
+ActiveRecord::Schema.define(version: 2019_05_02_112633) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_192237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.string "role", default: "buyer"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
