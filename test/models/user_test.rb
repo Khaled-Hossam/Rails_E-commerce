9f384e072d6a98baadf03792c55ec31f7c346d5f
Khaled Hossam <khaledhossam10@outlook.com>
2019-05-07 23:19:55 +0200

    feat(testing): fix testing for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  test 'invalid without email' do
+    user = User.new()
+    assert_not_nil user.errors[:email]
+  end
+
   test "the user email" do
     user = User.new(email: 'email@example.com')
     assert_equal 'email@example.com', user.email 
